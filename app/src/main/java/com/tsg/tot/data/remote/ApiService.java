c37f173452d76b3adbd7a3643c32e2526ecece90
Bryan Ortiz <bryansom1207@hotmail.com>
2020-08-25 23:58:12 -0500

    - Se cargan archivos adicionales

---
+++ app/src/main/java/com/tsg/tot/data/remote/ApiService.java
@@ -0,0 +1,18 @@
+package com.tsg.tot.data.remote;
+
+import com.tsg.tot.data.model.Student;
+import com.tsg.tot.data.model.Version;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface ApiService {
+
+    @GET("versiones.json")
+    Call<List<Version>> getVersion();
+
+    @GET("estudiantes.json")
+    Call<List<Student>> getStudent();
+}
---
+++ app/src/main/java/com/tsg/tot/data/remote/ApiUtils.java
@@ -0,0 +1,16 @@
+package com.tsg.tot.data.remote;
+
+public class ApiUtils {
+
+
+    private ApiUtils() {
+    }
+
+    static final String BASE_URL = "http://192.168.0.27:8000/api/gobernacion/cursos/";
+
+    public static ApiService getAPIService() {
+
+        return Client.getClient().create(ApiService.class);
+    }
+
+}
