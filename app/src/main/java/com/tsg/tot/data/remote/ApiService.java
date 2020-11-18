7ff176afb763af8b84ba5210e26ae3834208d610
Israel Martinez <ikis.leon.dunny@gmail.com>
2020-11-18 01:28:03 -0600

    Modificacion de llamado de servicios

--- app/src/main/java/com/tsg/tot/data/remote/ApiService.java
+++ app/src/main/java/com/tsg/tot/data/remote/ApiService.java
@@ -29,46 +29,60 @@ import retrofit2.http.POST;
  */
 public interface ApiService {
 
-    @GET("api/gobernacion/cursos/versiones.json")
+    //@GET("api/gobernacion/cursos/versiones.json")
+    @GET("api/gobernacion/cursos/versiones")
     Call<List<Version>> getVersion();
 
-    @GET("api/gobernacion/cursos/disposotivos.json")
+   // @GET("api/gobernacion/cursos/disposotivos.json")
+    @GET("api/gobernacion/cursos/disposotivos")
     Call<List<Device>> getDevice();
 
-    @GET("api/gobernacion/cursos/clases.json")
+    //@GET("api/gobernacion/cursos/clases.json")
+    @GET("api/gobernacion/cursos/clases")
     Call<List<Lessons>> getLessons();
 
-    @GET("api/gobernacion/cursos/grados.json")
+    //@GET("api/gobernacion/cursos/grados.json")
+    @GET("api/gobernacion/cursos/grados")
     Call<List<Grade>> getGrade();
 
-    @GET("api/gobernacion/cursos/ejercicios.json")
+    //@GET("api/gobernacion/cursos/ejercicios.json")
+    @GET("api/gobernacion/cursos/ejercicios")
     Call<List<Exercises>> getExercises();
 
-    @GET("api/gobernacion/cursos/entregas.json")
+    // @GET("api/gobernacion/cursos/entregas.json")
+    @GET("api/gobernacion/cursos/entregas")
     Call<List<Submissions>> getSubmissions();
 
-    @GET("api/gobernacion/cursos/estudiantes.json")
+    //@GET("api/gobernacion/cursos/estudiantes.json")
+    @GET("api/gobernacion/cursos/estudiantes")
     Call<List<Student>> getStudent();
 
-    @GET("api/gobernacion/cursos/evaluaciones.json")
+    //@GET("api/gobernacion/cursos/evaluaciones.json")
+    @GET("api/gobernacion/cursos/evaluaciones")
     Call<List<Evaluations>> getEvaluations();
 
-    @GET("api/gobernacion/cursos/materialestudio.json")
+    //@GET("api/gobernacion/cursos/materialestudio.json")
+    @GET("api/gobernacion/cursos/materialestudio")
     Call<List<StudyMaterial>> getStudyMaterial();
 
-    @GET("api/gobernacion/cursos/materias.json")
+    //@GET("api/gobernacion/cursos/materias.json")
+    @GET("api/gobernacion/cursos/materias")
     Call<List<Subjects>> getSubjects();
 
-    @GET("api/gobernacion/cursos/planeacion.json")
+    //@GET("api/gobernacion/cursos/planeacion.json")
+    @GET("api/gobernacion/cursos/planeacion")
     Call<List<Planning>> getPlanning();
 
-    @GET("api/gobernacion/cursos/profesores.json")
+    //@GET("api/gobernacion/cursos/profesores.json")
+    @GET("api/gobernacion/cursos/profesores")
     Call<List<Teacher>> getTeachers();
 
-    @GET("api/gobernacion/cursos/subidas.json")
+    //@GET("api/gobernacion/cursos/subidas.json")
+    @GET("api/gobernacion/cursos/subidas")
     Call<List<Upload>> getUploads();
 
-    @GET("api/gobernacion/cursos/tareas.json")
+    //@GET("api/gobernacion/cursos/tareas.json")
+    @GET("api/gobernacion/cursos/tareas")
     Call<List<Task>> getTasks();
 
     @POST("api/gobernacion/cursos/tareas/")
